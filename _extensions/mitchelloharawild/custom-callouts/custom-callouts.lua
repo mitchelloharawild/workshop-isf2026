7ae9e0c957536363e683e1b054c3babd795f2525
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2026-06-24 17:01:18 +0200

    Finish welcome slides

--- _extensions/mitchelloharawild/custom-callouts/custom-callouts.lua
+++ _extensions/mitchelloharawild/custom-callouts/custom-callouts.lua
@@ -2,7 +2,7 @@ local function ensure_html_deps()
   quarto.doc.add_html_dependency({
     name = "custom-callouts",
     version = "0.1.0",
-    stylesheets = {"resources/css/custom-callouts.css"}
+    stylesheets = {"resources/css/custom-callouts.scss"}
   })
 end
 if quarto.doc.is_format('html:js') then
